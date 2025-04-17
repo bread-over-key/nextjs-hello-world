4b5e7e8a5c9d0a9b98f5029e0c9be9db95c76be0
bread-over-key <possiblemishin@gmail.com>
2025-04-17 16:29:06 -0400

    updates

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,15 @@
+interface Post{
+
+	id: number
+	title: string
+
+}
 export default async function Page() {
   const data = await fetch('https://api.vercel.app/blog')
   const posts = await data.json()
   return (
     <ul>
-      {posts.map((post) => (
+      {posts.map((post: Post) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ul>
