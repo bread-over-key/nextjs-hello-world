d7a47871ac61e3901c23c4ff43f13f3360ec1256
bread-over-key <possiblemishin@gmail.com>
2025-05-26 17:19:45 -0400

    updates

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,8 @@ const Page = async () => {
 			<h1>Sign in</h1>
 			<a href="/login/github">Sign in with GitHub</a>
 
-			<button onClick={() => fetch('/api/logenv')}>Trigger Env Log</button>
+			<button onClick={() => fetch('/api/hello')}>Trigger Env Log</button>
+			<button>hello</button>
 		</>
 	);
 };
